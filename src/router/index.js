ecd84efd18a3cd5c3df74f8a17c186fe5ed92f52
zyh <zhangyinghaoweb@163.com>
2018-07-31 18:50:42 +0800

    login页面路由，及布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '@/views/Login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    
+    {name: 'Login', path: '/login', component: Login}
   ]
 })
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,73 @@
 <template>
-    
+    <div class="login">
+       <el-form ref="form" :model="form" class="container" :rules="rules">
+            <el-form-item class="avatar" >
+               <img src="../assets/avatar.jpg" alt="">
+            </el-form-item>
+            <el-form-item prop="username">
+                <el-input v-model="form.username" placeholder="账号" prefix-icon="myicon myicon-user"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+                <el-input v-model="form.password" placeholder="密码" prefix-icon="myicon myicon-key"></el-input>
+            </el-form-item>  
+             <el-form-item prop="password">
+                 <el-button type="primary" class="login-btn" >登录</el-button>
+            </el-form-item>  
+           
+       </el-form> 
+    </div> 
 </template>
 <script>
 export default{
-
+    data(){
+        return{
+            form: {
+                username: '',
+                password: ''
+            },
+            rules: {
+                username: [
+                    { required: true, message: '请输入用户名', trigger: 'blur' },
+                ],
+                password: [
+                    { required: true, message: '请输入密码', trigger: 'blur' },
+                ]
+            } 
+        }
+    }
 }
 </script>
-<style>
+<style lang="scss" scoped>
+.login {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: #2f4050;
 
+  .container {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 400px;
+    padding: 0px 40px 15px 40px;
+    margin: 200px auto;
+    background: white;
+    .avatar {
+      position: relative;
+      left: 50%;
+      width: 120px;
+      height: 120px;
+      margin-left: -60px;
+      margin-top: -60px;
+      box-sizing: border-box;
+      border-radius: 50%;
+      border: 10px solid #fff;
+      box-shadow: 0 1px 5px #ccc;
+      overflow: hidden;
+    }
+    .login-btn {
+      width: 100%;
+    }
+  }
+}
 </style>
