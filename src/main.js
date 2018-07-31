89e830488c2e2ab859cf7eec0f74437e5cc03a53
zyh <zhangyinghaoweb@163.com>
2018-07-31 15:04:18 +0800

    css init

--- src/main.js
+++ src/main.js
@@ -6,11 +6,11 @@ import router from './router'
 import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-import "@/styles/reset.css"
-import "@/styles/index.css"
-// 用来重置element-ui样式
-import "@/styles/element.css"
-import '@/styles/icon.css'
+ import "@/styles/reset.css"
+ import "@/styles/index.css"
+ // 用来重置element-ui样式
+ import "@/styles/element.css"
+ import "@/styles/icon.css"
 
 Vue.use(ElementUi)
 Vue.config.productionTip = false
